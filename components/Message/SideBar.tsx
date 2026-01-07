bd74867880a19b9f0cd871d365b924c09668236d
sagarcodewik <sagar.codewik@gmail.com>
2026-01-07 19:37:41 +0530

    Fixed All Chat Styling and HTML Code Issues in The Chat View.

--- components/Message/SideBar.tsx
+++ components/Message/SideBar.tsx
@@ -30,25 +30,25 @@ const SideBar = () => {
                     stroke-width="1.5"
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                  ></path>
+                  />
                   <path
                     d="M18.9299 20.6898C19.4599 22.2898 20.6699 22.4498 21.5999 21.0498C22.4499 19.7698 21.8899 18.7198 20.3499 18.7198C19.2099 18.7098 18.5699 19.5998 18.9299 20.6898Z"
                     stroke-width="1.5"
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                  ></path>
+                  />
                   <path
                     d="M14 5H20"
                     stroke-width="1.5"
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                  ></path>
+                  />
                   <path
                     d="M14 8H17"
                     stroke-width="1.5"
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                  ></path>
+                  />
                 </svg>
               </div>
               <input type="text" placeholder="Search Contact" />
@@ -56,22 +56,15 @@ const SideBar = () => {
           </div>
         </div>
       </div>
-
       <div className="msg-profiles-wrapper">
-        <div className="msg-profiles-container" msg-chat-contacts-wrapper>
-          <div
-            className={`msg-contact-box ${
-              activeChat === "james" ? "active" : ""
-            }`}
-            msg-chat-contact
-            onClick={() => setActiveChat("james")}
-          >
+        <div className="msg-profiles-container" msg-chat-contacts-wrapper="">
+          <div className="msg-contact-box" msg-chat-contact="" data-active="">
             <div className="contact-item">
               <div className="contact-avatar-wrapper">
                 <img
                   className="contact-avatar"
                   src="/images/profile-avatars/profile-avatar-6.jpg"
-                  alt="#"
+                  alt=""
                 />
                 <div className="contact-status-indicator online"></div>
               </div>
@@ -94,19 +87,13 @@ const SideBar = () => {
             </div>
           </div>
 
-          <div
-            className={`msg-contact-box ${
-              activeChat === "jerome" ? "active" : ""
-            }`}
-            msg-chat-contact
-            onClick={() => setActiveChat("jerome")}
-          >
+          <div className="msg-contact-box" msg-chat-contact="">
             <div className="contact-item">
               <div className="contact-avatar-wrapper">
                 <img
                   className="contact-avatar"
                   src="/images/profile-avatars/profile-avatar-28.jpg"
-                  alt="#"
+                  alt=""
                 />
                 <div className="contact-status-indicator online"></div>
               </div>
@@ -125,6 +112,62 @@ const SideBar = () => {
               </div>
             </div>
           </div>
+
+          <div className="msg-contact-box" msg-chat-contact="">
+            <div className="contact-item">
+              <div className="contact-avatar-wrapper">
+                <img
+                  className="contact-avatar"
+                  src="/images/profile-avatars/profile-avatar-27.jpg"
+                  alt=""
+                />
+                <div className="contact-status-indicator offline"></div>
+              </div>
+
+              <div className="contact-content">
+                <div className="contact-header">
+                  <h4 className="contact-name">Madagascar Silver</h4>
+                  <span className="contact-time">12:25</span>
+                </div>
+
+                <div className="contact-preview">
+                  <div className="contact-message">
+                    <p>Lorem ipsum, dolor sit a consectetur adipisicing eli</p>
+                  </div>
+                  <div className="contact-unread-count">
+                    <span>999</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="msg-contact-box" msg-chat-contact="">
+            <div className="contact-item">
+              <div className="contact-avatar-wrapper">
+                <img
+                  className="contact-avatar"
+                  src="/images/profile-avatars/profile-avatar-26.jpg"
+                  alt=""
+                />
+                <div className="contact-status-indicator offline"></div>
+              </div>
+
+              <div className="contact-content">
+                <div className="contact-header">
+                  <h4 className="contact-name">Pippins McGray</h4>
+                  <span className="contact-time">12:25</span>
+                </div>
+
+                <div className="contact-preview">
+                  <div className="contact-message">
+                    <p>Lorem ipsum, dolor sit a consectetur adipisicing eli</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
