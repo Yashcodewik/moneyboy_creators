ff888a9f192f8a7597a833f1e2caa92401189799
Yashcodewik <yashcodewik@gmail.com>
2026-02-10 15:40:45 +0530

    marketplace tab

--- components/StorePage/Index.tsx
+++ components/StorePage/Index.tsx
@@ -29,6 +29,9 @@ import { fetchAllCreators, fetchMyPaidPosts } from "@/redux/store/Action";
 import PPVRequestModal from "../ProfilePage/PPVRequestModal";
 import { useRouter } from "next/navigation";
 import { savePost, unsavePost } from "@/redux/other/savedPostsSlice";
+import { subscribeCreator, unlockPost } from "@/redux/Subscription/Action";
+import UnlockContentModal from "../ProfilePage/UnlockContentModal";
+import SubscriptionModal from "../ProfilePage/SubscriptionModal";
 
 type TimeFilter =
   | "all_time"
@@ -55,10 +58,14 @@ const StorePage = () => {
   const [selectedCreator, setSelectedCreator] = useState<any | null>(null);
   const activeStoreOwner = selectedCreator || session?.user;
   const [time, setTime] = useState<TimeFilter>("all_time");
-
+  const [unlockModalPost, setUnlockModalPost] = useState<any | null>(null);
   const playerRef = useRef<any>(null);
   const [open, setOpen] = useState(false);
   const dispatch = useDispatch<AppDispatch>();
+  const [showSubscriptionModal, setShowSubscriptionModal] = useState(false);
+  const [subscriptionPlan, setSubscriptionPlan] = useState<
+    "MONTHLY" | "YEARLY"
+  >("MONTHLY");
   const profilePublicId = activeStoreOwner?.publicId;
   const handleOpenFullscreen = (e: React.MouseEvent) => {
     e.preventDefault();
@@ -105,6 +112,21 @@ const StorePage = () => {
     if (creatorsPagination.page > 1) {
       dispatch(fetchAllCreators({ page: creatorsPagination.page - 1 }));
     }
+  };
+
+  const { unlocking } = useSelector((state: RootState) => state.subscription);
+
+  const handleConfirmUnlock = async () => {
+    if (!unlockModalPost) return;
+
+    await dispatch(
+      unlockPost({
+        postId: unlockModalPost._id,
+        creatorId: unlockModalPost.userId,
+      }),
+    );
+
+    setUnlockModalPost(null); // close modal
   };
 
   useEffect(() => {
@@ -167,6 +189,19 @@ const StorePage = () => {
         }),
       );
     }
+  };
+
+  const handleConfirmSubscription = async () => {
+    if (!activeStoreOwner?._id) return;
+
+    await dispatch(
+      subscribeCreator({
+        creatorId: activeStoreOwner._id,
+        planType: subscriptionPlan,
+      }),
+    );
+
+    setShowSubscriptionModal(false);
   };
 
   return (
@@ -547,7 +582,14 @@ const StorePage = () => {
                     <div className="moneyboy-special-content-banner--content">
                       <h2>Get exclusive access</h2>
                       <p>Subscribe to unlock everything from this creator.</p>
-                      <a href="#" className="btn-txt-gradient btn-outline">
+                      <a
+                        href="#"
+                        className="btn-txt-gradient btn-outline"
+                        onClick={() => {
+                          setSubscriptionPlan("MONTHLY"); // default
+                          setShowSubscriptionModal(true);
+                        }}
+                      >
                         <svg
                           className="only-fill-hover-effect"
                           xmlns="http://www.w3.org/2000/svg"
@@ -958,7 +1000,12 @@ const StorePage = () => {
                                                 !post.isSubscribed &&
                                                 post.accessType ===
                                                   "pay_per_view" && (
-                                                  <a className="btn-txt-gradient btn-outline">
+                                                  <a
+                                                    className="btn-txt-gradient btn-outline"
+                                                    onClick={() =>
+                                                      setUnlockModalPost(post)
+                                                    }
+                                                  >
                                                     <svg
                                                       className="only-fill-hover-effect"
                                                       xmlns="http://www.w3.org/2000/svg"
@@ -997,7 +1044,17 @@ const StorePage = () => {
                                                 !post.isSubscribed &&
                                                 post.accessType ===
                                                   "subscriber" && (
-                                                  <a className="btn-txt-gradient btn-outline grey-variant">
+                                                  <a
+                                                    className="btn-txt-gradient btn-outline grey-variant"
+                                                    onClick={() => {
+                                                      setSubscriptionPlan(
+                                                        "MONTHLY",
+                                                      ); // default
+                                                      setShowSubscriptionModal(
+                                                        true,
+                                                      );
+                                                    }}
+                                                  >
                                                     <svg
                                                       xmlns="http://www.w3.org/2000/svg"
                                                       width="20"
@@ -1199,7 +1256,12 @@ const StorePage = () => {
                                               !post.isSubscribed &&
                                               post.accessType ===
                                                 "pay_per_view" && (
-                                                <a className="btn-txt-gradient btn-outline">
+                                                <a
+                                                  className="btn-txt-gradient btn-outline"
+                                                  onClick={() =>
+                                                    setUnlockModalPost(post)
+                                                  }
+                                                >
                                                   <svg
                                                     className="only-fill-hover-effect"
                                                     xmlns="http://www.w3.org/2000/svg"
@@ -1238,7 +1300,17 @@ const StorePage = () => {
                                               !post.isSubscribed &&
                                               post.accessType ===
                                                 "subscriber" && (
-                                                <a className="btn-txt-gradient btn-outline grey-variant">
+                                                <a
+                                                  className="btn-txt-gradient btn-outline grey-variant"
+                                                  onClick={() => {
+                                                    setSubscriptionPlan(
+                                                      "MONTHLY",
+                                                    ); // default
+                                                    setShowSubscriptionModal(
+                                                      true,
+                                                    );
+                                                  }}
+                                                >
                                                   <svg
                                                     xmlns="http://www.w3.org/2000/svg"
                                                     width="20"
@@ -1333,6 +1405,42 @@ const StorePage = () => {
           </div>
         </div>
       </div>
+
+      {showSubscriptionModal && (
+        <SubscriptionModal
+          onClose={() => setShowSubscriptionModal(false)}
+          onConfirm={handleConfirmSubscription}
+          plan={subscriptionPlan}
+          action="subscribe"
+          creator={{
+            displayName: activeStoreOwner?.displayName,
+            userName: activeStoreOwner?.userName,
+            profile: activeStoreOwner?.profile,
+          }}
+          subscription={{
+            monthlyPrice: activeStoreOwner?.subscription?.monthlyPrice,
+            yearlyPrice: activeStoreOwner?.subscription?.yearlyPrice,
+          }}
+        />
+      )}
+
+      {unlockModalPost && (
+        <UnlockContentModal
+          onClose={() => setUnlockModalPost(null)}
+          creator={{
+            displayName: activeStoreOwner?.displayName,
+            userName: activeStoreOwner?.userName,
+            profile: activeStoreOwner?.profile,
+          }}
+          post={{
+            publicId: unlockModalPost.publicId,
+            text: unlockModalPost.text,
+            price: unlockModalPost.price,
+          }}
+          onConfirm={handleConfirmUnlock}
+          loading={unlocking}
+        />
+      )}
 
       {showPPVModal && selectedCreator && (
         <PPVRequestModal
---
+++ redux/Subscription/Action.ts
@@ -0,0 +1,69 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import defaultAxios from "@/utils/api/axios";
+import { API_UNLOCK_POST, API_SUBSCRIBE_CREATOR } from "@/utils/api/APIConstant";
+
+/* ------------------- Unlock Post (PPV) ------------------- */
+export const unlockPost = createAsyncThunk(
+  "subscription/unlockPost",
+  async (
+    {
+      postId,
+      creatorId,
+    }: {
+      postId: string;
+      creatorId: string;
+    },
+    { rejectWithValue }
+  ) => {
+    try {
+      const res = await defaultAxios.post(API_UNLOCK_POST, {
+        postId,
+        creatorId,
+      });
+
+      if (!res?.data?.success) {
+        return rejectWithValue("Failed to unlock post");
+      }
+
+      return res.data; // { success, isUnlocked, postId }
+    } catch (error: any) {
+      return rejectWithValue(
+        error?.response?.data?.message || "Something went wrong"
+      );
+    }
+  }
+);
+
+/* ------------------- Subscribe Creator ------------------- */
+export const subscribeCreator = createAsyncThunk(
+  "subscription/subscribeCreator",
+  async (
+    {
+      creatorId,
+      planType,
+    }: {
+      creatorId: string;
+      planType: "MONTHLY" | "YEARLY";
+    },
+    { rejectWithValue }
+  ) => {
+    try {
+      const res = await defaultAxios.post(API_SUBSCRIBE_CREATOR, {
+        creatorId,
+        planType,
+      });
+
+      if (!res?.data?.success) {
+        return rejectWithValue(
+          res?.data?.message || "Failed to subscribe"
+        );
+      }
+
+      return res.data; // { success, data: purchase }
+    } catch (error: any) {
+      return rejectWithValue(
+        error?.response?.data?.message || "Something went wrong"
+      );
+    }
+  }
+);
