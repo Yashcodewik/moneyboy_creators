8b7b55b4e1a082658247cb693f8e5b23555b3d12
sagarcodewik <sagar.codewik@gmail.com>
2026-02-26 18:56:08 +0530

    D

--- utils/constance.ts
+++ utils/constance.ts
@@ -8,10 +8,10 @@
 // export const socketBaseUrl = "http://192.168.1.36:1999/";
 
 // Mir
-export const APIBaseUrl = "http://192.168.1.27:1999/api/";
-export const ImageBaseUrl = "http://192.168.1.27:1999/";
-export const socketBaseUrl = "http://192.168.1.27:1999/";
+// export const APIBaseUrl = "http://192.168.1.27:1999/api/";
+// export const ImageBaseUrl = "http://192.168.1.27:1999/";
+// export const socketBaseUrl = "http://192.168.1.27:1999/";
 
-// export const APIBaseUrl = "https://api.moneyboy.com/api/";
-// export const frontendURL = "https://moneyboy.com";
-// export const socketBaseUrl = "https://api.moneyboy.com";
+export const APIBaseUrl = "https://api.moneyboy.com/api/";
+export const frontendURL = "https://moneyboy.com";
+export const socketBaseUrl = "https://api.moneyboy.com";
